35bb50557a27a23d24080da8fce9090ae0246aa5
TTToh724christ <Timothy12christ>
2019-12-07 14:33:47 +0800

    Visual 1

---
+++ Tim11-16-3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 16 15:09:51 2019
+
+@author: ASUS
+"""
+
+y=int(input('How old are you?'))
+if y<9:
+    print('You are primmary level ')
+elif y>=9 and y<=10:
+    print('You are middle grades')
+else:
+    print('You senior grades')
